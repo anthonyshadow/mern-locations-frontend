7f51827119082fbedd6e4cde8aa533cd830273c2
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-07-24 16:07:20 -0400

    able to delete a place

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -4,13 +4,17 @@ import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button';
 import Modal from '../../shared/components/UIElements/Modal';
 import Map from '../../shared/components/UIElements/Map';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import { AuthContext } from '../../shared/context/auth-context';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 import './PlaceItem.css';
 
 const PlaceItem = props => {
   const auth = useContext(AuthContext);
   const [showMap, setShowMap] = useState(false);
   const [showConfirmModal, setShowConfirmModal] = useState(false);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
   const openMapHandler = () => setShowMap(true);
 
@@ -24,13 +28,17 @@ const PlaceItem = props => {
     setShowConfirmModal(false);
   };
 
-  const confirmDeleteHandler = () => {
+  const confirmDeleteHandler = async () => {
     setShowConfirmModal(false);
-    console.log('DELETING...');
+    try{
+      await sendRequest(`http://localhost:5001/api/places/${props.id}`, 'DELETE');
+      props.onDelete(props.id);
+    } catch (err) {}
   };
 
   return (
     <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
       <Modal
         show={showMap}
         onCancel={closeMapHandler}
@@ -66,6 +74,7 @@ const PlaceItem = props => {
       </Modal>
       <li className="place-item">
         <Card className="place-item__content">
+          {isLoading && <LoadingSpinner/>}
           <div className="place-item__image">
             <img src={props.image} alt={props.title} />
           </div>
--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -25,6 +25,10 @@ const UserPlaces = () => {
     fetchUserPlaces();
   }, [sendRequest, userId]);
 
+  const placeDeletedHandler = deletedPlaceId => {
+    setLoadedPlaces(prevPlaces => prevPlaces.filter(place => place.id !== deletedPlaceId ))
+  }
+
   return (
     <React.Fragment>
       <ErrorModal error={error} onClear={clearError} />
@@ -33,7 +37,7 @@ const UserPlaces = () => {
           <LoadingSpinner />
         </div>
       )}
-      {!isLoading && loadedPlaces && (<PlaceList items={loadedPlaces} />)}
+      {!isLoading && loadedPlaces && (<PlaceList items={loadedPlaces} onDeletePlace={placeDeletedHandler}/>)}
     </React.Fragment>
   );
 };

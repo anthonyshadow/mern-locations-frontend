b19da8bdd3005e0706783c75e6bf12ea2a6b458d
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-07-25 15:40:54 -0400

    clean up code

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -52,7 +52,6 @@ const NewPlace = () => {
       formData.append("description", formState.inputs.description.value);
       formData.append("address", formState.inputs.address.value);
       formData.append("image", formState.inputs.image.value);
-      formData.append("creator", auth.userId);
       await sendRequest("http://localhost:5001/api/places", "POST", formData, {
         Authorization: "Bearer " + auth.token,
       });

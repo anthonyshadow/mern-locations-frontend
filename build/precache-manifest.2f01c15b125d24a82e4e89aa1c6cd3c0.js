79267ea90b302ddfa6fb67526a8793cbd5aa474a
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-07-26 14:02:56 -0400

    add code splitting

---
+++ build/precache-manifest.2f01c15b125d24a82e4e89aa1c6cd3c0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0e0c60bec03c158c885d6e1ae30e16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a96c55cdb6c493665ba1",
+    "url": "/static/css/0.c4efc930.chunk.css"
+  },
+  {
+    "revision": "8eeb86bac0ce85fdf149",
+    "url": "/static/css/4.1e5f2690.chunk.css"
+  },
+  {
+    "revision": "aece6955f8734c7de7f2",
+    "url": "/static/css/5.39533c54.chunk.css"
+  },
+  {
+    "revision": "e4a616b0bc39e1926220",
+    "url": "/static/css/6.66a80850.chunk.css"
+  },
+  {
+    "revision": "2a6a36403051975bfd96",
+    "url": "/static/css/7.85643802.chunk.css"
+  },
+  {
+    "revision": "24e399bcd8147c2dbe3b",
+    "url": "/static/css/8.749363a7.chunk.css"
+  },
+  {
+    "revision": "226e71d6c14015f36cbf",
+    "url": "/static/css/main.65269f5a.chunk.css"
+  },
+  {
+    "revision": "a96c55cdb6c493665ba1",
+    "url": "/static/js/0.247ab567.chunk.js"
+  },
+  {
+    "revision": "a5864c6168d7b5fca15c",
+    "url": "/static/js/3.84f2b127.chunk.js"
+  },
+  {
+    "revision": "8eeb86bac0ce85fdf149",
+    "url": "/static/js/4.88f9f487.chunk.js"
+  },
+  {
+    "revision": "aece6955f8734c7de7f2",
+    "url": "/static/js/5.c2ba1ea6.chunk.js"
+  },
+  {
+    "revision": "e4a616b0bc39e1926220",
+    "url": "/static/js/6.1f0207d7.chunk.js"
+  },
+  {
+    "revision": "2a6a36403051975bfd96",
+    "url": "/static/js/7.a7b4f122.chunk.js"
+  },
+  {
+    "revision": "24e399bcd8147c2dbe3b",
+    "url": "/static/js/8.424ef533.chunk.js"
+  },
+  {
+    "revision": "226e71d6c14015f36cbf",
+    "url": "/static/js/main.716f0fea.chunk.js"
+  },
+  {
+    "revision": "2c628ffc5f3578818f90",
+    "url": "/static/js/runtime-main.3010e3fc.js"
+  }
+]);
